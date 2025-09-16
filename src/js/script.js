8c93b0e49b838c6d04cfbfa4275424dc649dde6d
ninhatzo <anac.estudantes2@gmail.com>
2025-09-15 22:03:51 -0300

    Adicionando maior que, menor que, soma e média

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,18 @@
 // VALIDANDO SE O FORMULÁRIO ESTÁ VAZIO
 function validaForm() {
 
-        // ARRAYS DE ARMAZENAMENTO
-        let contador = 0;
-        let maiorNumero = 0;
-        let menorNumero = 0;
-        let somaTodos = 0;
-        let media = 0;
+    // ARRAYS DE ARMAZENAMENTO
+    let contador = 0;
+    let maiorNumero = 0;
+    let menorNumero = 0;
+    let somaTodos = 0;
+    let media = 0;
 
-        if(document.form.txtnum.value=="") {
-            alert("Preencha o campo do número!");
-           document.form.txtnum.focus();
-           return false;
-            } else {
+    if(document.form.txtnum.value=="") {
+        alert("Preencha o campo do número!");
+       document.form.txtnum.focus();
+        return false;
+        } else {
 
             num = parseInt(document.form.txtnum.value);
 
@@ -46,11 +46,12 @@ function validaForm() {
                 }
                 
             }
-        }
+    }
+    
         console.log("Quantidade de números digitados: ", contador);
         console.log("Maior número inserido:", maiorNumero);
         console.log("Menor número inserido:", menorNumero);
         console.log("Soma Todos Números:", somaTodos);
-        console.log("Media: ",media);
+        console.log("Media: ", media);
         return false;
 }
